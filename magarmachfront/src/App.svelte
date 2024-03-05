ce72182f48a16f3f84edbc1cb514943b46ec719e
xorvet <cypherx702@gmail.com>
2024-03-05 08:56:14 +0530

    update

---
+++ magarmachfront/src/App.svelte
@@ -0,0 +1,14 @@
+<main class="flex flex-col font-mono w-full h-full bg-black">
+  <div class="h-screen bg-[url(/magarnobg.webp)] bg-no-repeat bg-center bg-black">
+    <nav class="text-2xl flex w-full h-[10vh] justify-around p-6 font-medium">
+      <a href="">Home</a>
+      <a href="">How it works?</a>
+      <a href="">Lookup</a>
+      <a href="">Sign</a>
+      <a href="">create account</a>
+    </nav>
+    <p class="text-4xl text-center italic mx-auto mt-64 w-full py-6 bg-neutral/20 border-y-2 border-neutral">guard your documents with our magarmach</p>
+  </div>
+  <div class="border-t-2 border-neutral h-screen">
+  </div>
+</main>
